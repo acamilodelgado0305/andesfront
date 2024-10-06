88992fa5cd5f36f16aa80dc1bf1bc25cda1b04a2
Camilo Delgado <camilodelgado0305@gmail.com>
2024-10-06 12:02:16 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: ['https://fevaback.app.validaciondebachillerato.com.co'],
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['jwt-decode'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Agrupar dependencias de node_modules en un chunk 'vendor'
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          // Puedes agregar más lógica aquí si es necesario
-        },
-      },
-    },
-    chunkSizeWarningLimit: 2000, // Aumentar el límite de advertencia del tamaño del chunk
-  },
-});
+  base: '/',  // Si está en la raíz, no hay necesidad de cambiar el base.
+})
