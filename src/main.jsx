d9fe235f438185951a574fb916c3093e12ce5dd8
Andres <TuEmail@example.com>
2025-01-25 16:34:22 -0500

    Formulario Camilo Delgado

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Programs from "./components/programas/programas";
 import Materias from "./components/materias/Materias";
 import CamiloForm from "./components/PublicForms/formCamilo";
 import StudentRegistrationForm from "./components/PublicForms/formAdriana";
+import StudentRegistrationForm1 from "./components/PublicForms/formCamilo";
 
 // Contexto de autenticación
 import { AuthProvider } from "./AuthContext";
@@ -31,7 +32,7 @@ const App = () => (
         <Routes>
           {/* Rutas públicas */}
           <Route path="/" element={<Landing />} />
-          <Route path="/register/c" element={<CamiloForm />} />
+          <Route path="/Camilodelgado" element={<StudentRegistrationForm1 />} />
           <Route path="/Adrianabenitez" element={<StudentRegistrationForm />} />
 
           {/* Rutas principales */}
